718fc011f06f9335f1bd20949cbe46a6d0b3fe99
Samuel-Martin23 <martin.samuel.isaac@gmail.com>
2023-04-29 16:20:58 -0700

    New User works

--- src/main/java/com/example/effortlogger/AdminControl.java
+++ src/main/java/com/example/effortlogger/AdminControl.java
@@ -5,15 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Base64;
 
 public class AdminControl {
     /**
@@ -29,6 +32,11 @@ public class AdminControl {
     @FXML
     private ComboBox<String> comboBoxRoles;
 
+    @FXML
+    private Label adminErrorMsg;
+
+    String fileName = "credentials.csv";
+
     public void changeSceneMainMenu(ActionEvent event) throws IOException {
         Parent adminViewParent = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         Scene adminViewScene = new Scene(adminViewParent);
@@ -39,12 +47,12 @@ public class AdminControl {
         window.show();
     }
 
-
     /*
-    =========================
-    =========================
+    ==============================
+    Beginning of New User Scene
+    ==============================
     */
-    public void changeAdminSceneNewUserButtonPushed(ActionEvent event) throws IOException {
+    public void changeSceneAdminNewUser(ActionEvent event) throws IOException {
         Parent adminNewUserViewParent = FXMLLoader.load(getClass().getResource("AdminControlNewUser.fxml"));
         Scene adminNewUserViewScene = new Scene(adminNewUserViewParent);
 
@@ -54,9 +62,30 @@ public class AdminControl {
         window.show();
     }
 
-    public void changeSceneApplyButtonPushed(ActionEvent event) throws IOException {
-        System.out.println(newUserNameTextField.getText());
-        System.out.println(newUserPasswordField.getText());
+    public void changeSceneAdminNewUserApplyButtonPushed(ActionEvent event) throws IOException {
+
+        if (newUserNameTextField.getText().isEmpty()) {
+            adminErrorMsg.setVisible(true);
+            adminErrorMsg.setText("Name is empty.");
+            return;
+        }
+        if (doesNameExist(newUserNameTextField.getText())) {
+            adminErrorMsg.setVisible(true);
+            adminErrorMsg.setText("Name already exists.");
+            return;
+        }
+        if (newUserPasswordField.getText().isEmpty()) {
+            adminErrorMsg.setVisible(true);
+            adminErrorMsg.setText("Password is empty.");
+            return;
+        }
+        if (!(isValidPassword(newUserPasswordField.getText()))) {
+            adminErrorMsg.setVisible(true);
+            adminErrorMsg.setText("Incorrect password credentials.");
+            return;
+        }
+
+        writeToFile(newUserNameTextField.getText(), newUserPasswordField.getText(), comboBoxRoles.getValue());
 
         Parent adminViewParent = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         Scene adminViewScene = new Scene(adminViewParent);
@@ -66,4 +95,67 @@ public class AdminControl {
         window.setScene(adminViewScene);
         window.show();
     }
+    /*
+    ==============================
+    End of New User Scene
+    ==============================
+    */
+
+    /*
+    ==============================
+    Beginning of Helper Functions
+    ==============================
+    */
+    public boolean doesNameExist(String newUserName)
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                String[] columns = line.split(",");
+
+                // Check if the line has the expected structure: "name,password,role"
+                if (columns.length != 3) {
+                    continue;
+                }
+
+                if (newUserName.equals(columns[0].trim())) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public boolean isValidPassword(String password) {
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
+        return password.matches(regex);
+    }
+
+    public void writeToFile(String name, String password, String role) {
+        String line = name + "," + encryptPassword(password) + "," + role;
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+            writer.write(line);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error appending data to file: " + e.getMessage());
+        }
+    }
+
+    private String encryptPassword(String password) {
+        byte[] bytes = password.getBytes();
+        byte[] encryptedBytes = Base64.getEncoder().encode(bytes);
+        return new String(encryptedBytes);
+    }
+    /*
+    ==============================
+    End of Helper Functions
+    ==============================
+    */
 }
