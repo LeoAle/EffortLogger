23b4d463f20b79ccda46618f0ccf8f0966f00714
LeoAle <Efren0514@gmail.com>
2023-04-29 01:17:07 -0700

    Overhaul

--- src/main/java/com/example/effortlogger/AdminControl.java
+++ src/main/java/com/example/effortlogger/AdminControl.java
@@ -22,4 +22,6 @@ public class AdminControl {
         window.setScene(adminViewScene);
         window.show();
     }
+
+
 }
---
+++ src/main/java/com/example/effortlogger/EffortConsole.java
@@ -0,0 +1,5 @@
+package com.example.effortlogger;
+
+public class EffortConsole {
+
+}
